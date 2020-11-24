ee927c37339d1ece571151302af97425471be661
Navneet Pandey <navneetpandey9491@gmail.com>
2020-11-24 11:48:48 +0000

    Add files via upload

---
+++ python assitant.py
@@ -0,0 +1,98 @@
+import time
+
+answer =input ("hello! : ")
+
+if answer == "hi":
+  print("good morning sir ! ")
+
+
+for pause_in_second in [2]:
+    time.sleep(pause_in_second )
+    print("My name is Jarvis ")
+    time.sleep(pause_in_second )
+    print("And i am your AI assitant")
+    time.sleep(pause_in_second )
+
+
+
+ 
+answer == input ("shall i do something for you ??: ")
+if answer == "yes":
+  time.sleep(pause_in_second )
+  print("ok so what shall i do ?? :")
+else:
+ answer = input ("shall i quit...? ")
+if answer == "yes":
+    time.sleep(pause_in_second )
+    print("bye bye sir see you later ...!")
+
+
+else:
+ print("what should i do for you.... ??")
+
+
+answer = input ("shall i open youtube ? :")
+time.sleep(pause_in_second )
+if answer == "yes":
+          print("wait 1 sec...!")
+          time.sleep(pause_in_second )
+
+else:
+        
+      answer = input ("shall i quit...? ")
+      if answer == "yes":
+          print("bye bye sir see you later ...!")
+
+
+answer = input("are you conneted to inertnet....? :")
+if answer == "yes":
+          print("ok !")
+          print("please wait !")
+          time.sleep(pause_in_second )
+          print("loading....10%.....!")
+          time.sleep(pause_in_second )
+          print("loading....50%.....!")
+          time.sleep(pause_in_second )
+          print("loading....90%.....!")
+          time.sleep(pause_in_second )
+          print("loading....99%.....!")
+          time.sleep(pause_in_second )
+          print("Done...............!")
+          print("https://youtube.com/")
+
+    
+
+else:
+        
+
+          print("Ooops you are not conneted to inernet !")
+          print("try again")
+
+answer = input ("shall i quit...? ")
+if answer == "yes":
+    time.sleep(pause_in_second )
+    print("bye bye sir see you later ...!")
+
+
+else:
+ print("what should i do for you.... ??")
+
+
+answer = input ("shall i show the time :") 
+if answer == "yes":
+ for pause_in_second in [2]:
+         print(time.ctime())
+         time.sleep(pause_in_second )  
+
+answer = input ("have done your assignment ??")
+if answer == "yes":
+    time.sleep(pause_in_second )
+    print(" So let's play !")
+    time.sleep(pause_in_second )
+
+else:
+    print("OK! then do your assignment ")
+    time.sleep(pause_in_second )
+     
+
+
